9a93ad6f1ae3a1e448100340fde65e74a4500ea9
Vyacheslav Muravyev <neobuxabuse@gmail.com>
2023-11-08 23:34:07 +0300

    Add display of the backend validation messages on the login page, remove previous validation rule on minimal length in the passwordInput component

--- src/components/passwordInput/index.tsx
+++ src/components/passwordInput/index.tsx
@@ -1,37 +1,29 @@
 import {Form, Input} from 'antd'
 import {NamePath} from 'antd/es/form/interface'
 
+type ValidateStatus = Parameters<typeof Form.Item>[0]['validateStatus'];
+
 type Props = {
     name: string;
     placeholder: string;
-    dependencies?: NamePath[]
+    dependencies?: NamePath[],
+    validateStatus?: ValidateStatus
+    errorMsg?: string
 }
 
 export const PasswordInput = ({
                                   name,
                                   placeholder,
-                                  dependencies
+                                  dependencies,
+                                  validateStatus,
+                                  errorMsg
                               }: Props) => {
     return (
         <Form.Item
             name={name}
             dependencies={dependencies}
-            rules={[
-                {
-                    required: true,
-                    message: 'Обязательное поле'
-                },
-                ({getFieldValue}) => ({
-                    validator(_, value) {
-                        if (value.length < 6) {
-                            return Promise.reject(new Error('Пароль должен быть длиннее 6-ти символов'))
-                        } else {
-                            return Promise.resolve()
-                        }
-                    }
-                })
-            ]}
-            hasFeedback
+            help={errorMsg}
+            validateStatus={validateStatus}
         >
             <Input.Password placeholder={placeholder} size='large'></Input.Password>
         </Form.Item>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,23 +5,22 @@ import {CustomInput} from "../../components/customInput";
 import {PasswordInput} from "../../components/passwordInput";
 import {CustomButton} from "../../components/customButton";
 import {useLoginMutation, User} from "../../app/services/auth";
-import {ErrorAlert} from "../../components/errorAlert";
 import {useNavigate} from "react-router-dom";
 import {Paths} from "../../Paths";
+import {ErrorAlert} from "../../components/errorAlert";
 
 export const Login = () => {
     const navigate = useNavigate()
     const [loginUser] = useLoginMutation()
-    const [error, setError] = useState('')
+    const [error, setError] = useState<any>()
     const onLogin = async (data: User) => {
         try {
             await loginUser(data).unwrap();
             navigate(Paths.home)
         } catch (err) {
-            if (typeof err === "object" && err !== null && 'status' in err) {
-                if (err.status === 400) {
-                    setError('Неправильный логин или пароль')
-                }
+            console.log(err)
+            if (typeof err === "object" && err !== null && 'data' in err) {
+                setError(err.data)
             } else {
                 setError('Неизвестная ошибка')
             }
@@ -37,17 +36,21 @@ export const Login = () => {
                             type='text'
                             name='username'
                             placeholder='Имя пользователя'
+                            errorMsg={error?.username}
+                            validateStatus={error?.username ? 'error' : 'validating'}
                         />
                         <PasswordInput
                             name='password'
                             placeholder='Пароль'
+                            errorMsg={error?.password}
+                            validateStatus={error?.password ? 'error' : 'validating'}
                         />
                         <CustomButton
                             type="primary"
                             htmlType='submit'
                         > Войти
                         </CustomButton>
-                        <ErrorAlert message={error}/>
+                        <ErrorAlert message={error?.non_field_errors ? error['non_field_errors'] : ''}/>
                     </Form>
                 </Card>
             </Row>
